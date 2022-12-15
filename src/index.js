401222aa2cfa358a12477486bbe9461c90883444
RupertoRolo <romojulian69@gmail.com>
2022-12-14 21:29:24 -0500

    uwu

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const DATA = [
+  { id: "todo-0", name: "Messi", completed: true },
+  { id: "todo-1", name: "gana", completed: false },
+  { id: "todo-2", name: "la copa del mundo", completed: false },
+  { id: "todo-3", name: "y yo gano plata", completed: false }
+  
+  ];
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App tasks={DATA} />
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
